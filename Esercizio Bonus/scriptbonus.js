d691313e2752dc6059d46a0e2a94391bd6318b0b
DavidZarfati <zarda2001@gmail.com>
2025-10-30 15:29:24 +0100

    feat:Snack 2

--- Esercizio Bonus/scriptbonus.js
+++ Esercizio Bonus/scriptbonus.js
@@ -1,7 +1,22 @@
-let numero = parseInt(prompt("Dammi un numero"))
-if (numero % 2 === 0) {
-    console.log(numero)
+//////////COMMENTO ESERCIZI FINITI PER ALLEGGIRE LO SVOLGIMENTO DEL LIVE SERVER///////////////
+
+
+// let numero = parseInt(prompt("Dammi un numero"))
+// if (numero % 2 === 0) {
+//     console.log(numero)
+// }
+// else {
+//     console.log(numero + 1)
+// }
+
+let parola1 = prompt("inserisci una parola")
+let parola2 = prompt("inserisci un'altra parola")
+if (parola1.length > parola2.length) {
+    console.log(parola2, parola1)
 }
-else {
-    console.log(numero + 1)
+else if (parola1.length < parola2.length) {
+    console.log(parola1, parola2)
+}
+else if (parola1.length === parola2.length) {
+    console.log("Le tue parole hanno la stessa lunghezza")
 }
